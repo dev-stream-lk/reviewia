d151c5bc3fae6b1c89996718336d0b93d6b94230
UdithaIshan <uditha.ishan34@gmail.com>
2021-06-01 14:59:23 +0530

    initialized the project

---
+++ src/main/java/com/reviewia/reviewiaapi/ReviewiaApiApplication.java
@@ -0,0 +1,13 @@
+package com.reviewia.reviewiaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewiaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewiaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reviewia/reviewiaapi/ReviewiaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reviewia.reviewiaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewiaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
