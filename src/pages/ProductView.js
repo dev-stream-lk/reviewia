be21f383fe705d642ee749f08b085e44f4a19ddb
UdithaIshan <uditha.ishan34@gmail.com>
2021-07-10 13:37:59 +0530

    Product view page created

--- src/pages/ProductView.js
+++ src/pages/ProductView.js
@@ -1,77 +1,156 @@
-import { Container, Grid, makeStyles, Typography, Paper, Box } from "@material-ui/core";
+import {  Grid, makeStyles, Typography, Button, IconButton, Tooltip, } from "@material-ui/core";
 import React from "react";
-import Button from "../components/basic/Button";
 import Controls from "../components/Controls";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import Review from "../components/Review";
+import FavoriteBorderSharp from "@material-ui/icons/FavoriteBorderSharp";
+import CompareSharp from "@material-ui/icons/CompareSharp";
+import GroupAddSharp from "@material-ui/icons/GroupAddSharp";
+import Rating from "@material-ui/lab/Rating";
+import { Skeleton } from "@material-ui/lab";
 
-const useStyles = makeStyles( theme => ({
-  root: {
-    flexGrow: 1,
-    spacing:0,
-    direction:"row",
-    alignContent:"center",
-    justify:"center",
+const useStyles = makeStyles((theme) => ({
+  mainDiv: {
+    margin: theme.spacing(5),
   },
-  wrapper:{
-      marginTop: theme.spacing(5),
-      marginLeft: 0,
-      marginRight: 0,
-      alignContent: "space-between"
+  reviewContainer: {
+    margin: theme.spacing(2),
+    padding: theme.spacing(2),
   },
-  container: {
-      margin: theme.spacing(0),
-      padding: theme.spacing(4),
+  productContainer: {
+    textAlign: "start",
   },
-  paper: {
-      marginTop: theme.spacing(10),
-      // borderRadius:"10px",
-      // overflow:"hidden",
-      // boxShadow:"0px 0px 5px 2px rgba(0,0,0,0.21)",
-  },
-}))
+}));
 
 const ProductView = () => {
-
   const classes = useStyles();
 
   return (
-  <Container className={classes.root}>
-  <Grid container className={classes.wrapper} spacing="5">
-   <Grid item xs={12} sm={4}>
-   
-      <Grid container justify="center">
-      <Grid item xs={12}>
-        <Paper alig>s</Paper>
-      </Grid>
-      <Grid item xs={12}>
-        <Controls.Paper>s</Controls.Paper>
-      </Grid>
-      </Grid>
+    <div>
+      <Header />
+      <div className={classes.mainDiv}>
+        <Grid container className={classes.productContainer}>
+          {/* LHS */}
+          <Grid item xs={12} sm={5}>
+            <Grid container justify="center">
+              <Grid
+                item
+                xs={12}
+                style={{ display: "flex", justifyContent: "center" }}
+              >
+                <Skeleton width={400} height={400} />
+              </Grid>
+              <Grid item xs={12} style={{ textAlign: "center" }}>
+                <Rating />
+              </Grid>
+              <Grid item xs={12} style={{ textAlign: "center" }}>
+                <Typography variant="h5">4.7</Typography>
+              </Grid>
+              <Grid item xs={12} style={{ textAlign: "center" }}>
+              <Tooltip title="Add to Favourites" aria-label="add" arrow>
+              <IconButton
+                  color="primary"
+                  aria-label="Add to favourite"
+                  component="span"
+                >
+                  <FavoriteBorderSharp />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="Compare" aria-label="add" arrow>
+              <IconButton
+                  color="primary"
+                  aria-label="Add to favourite"
+                  component="span"
+                >
+                  <CompareSharp />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="Create Instant Group" aria-label="add" arrow>
+              <IconButton
+                  color="primary"
+                  aria-label="Add to favourite"
+                  component="span"
+                >
+                  <GroupAddSharp />
+                </IconButton>
+              </Tooltip>
+              </Grid>
+              <Grid item xs={9} direction="column">
+                <Controls.Paper>Brand:</Controls.Paper>
+                <Controls.Paper>Year:</Controls.Paper>
+                <Controls.Paper>Review count:</Controls.Paper>
+              </Grid>
+            </Grid>
+          </Grid>
 
-   </Grid>
-    
-   <Grid item xs={12} sm={8}>
-     <Grid container   >
-       <Grid item xs={6}><Typography variant="h5">Product Name</Typography></Grid>
-       <Grid item xs={6}><Button></Button></Grid>
-       <Grid item xs={12}>
-         <Paper className={useStyles.paper}>
-           <Grid container className={classes.container}>
-             <Grid item xs={1}><Typography variant="h5">Reviews</Typography></Grid>
-             <Grid item xs={12} style={{maxHeight: 'auto', height: '70vh', overflow: 'auto', paddingTop: '10px'}}>
-                <Review/>
-               <Review/>
-               <Review/>
-               <Review/>
-             </Grid>
-           </Grid>
-         </Paper>
-       </Grid>
-     </Grid>
-   </Grid>
- </Grid>
- </Container>
- )
+          {/* RHS */}
+          <Grid item xs={12} sm={7}>
+            <Grid container style={{ textAlign: "center" }}>
+              <Grid item xs={6}>
+                <Typography variant="h5">Product Name</Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <Button variant="outlined">Write a review</Button>
+              </Grid>
+              <Grid item xs={12}>
+                <Controls.Paper className={useStyles.paper}>
+                  <Grid container className={classes.reviewContainer}>
+                    <Grid item xs={1}>
+                      <Typography variant="h5">Reviews</Typography>
+                    </Grid>
+                    <Grid container>
+                      <Grid
+                        item
+                        xs={12}
+                        style={{
+                          // maxHeight: "100vh",
+                          height: "75vh",
+                          overflow: "auto",
+                          paddingTop: "10px",
+                        }}
+                      >
+                        <Review
+                          description="body1. Lorem ipsum dolor sit amet, consectetur adipisicing
+                                      elit. Quos blanditiis tenetur unde suscipit, quam beatae
+                                      rerum inventore consectetur, neque doloribus, cupiditate
+                                      numquam dignissimos laborum fugiat deleniti? Eum quasi
+                                      quidem quibusdam."
+                        />
+                        <Review
+                          description="body1. Lorem ipsum dolor sit amet, consectetur adipisicing
+                                      elit. Quos blanditiis tenetur unde suscipit, quam beatae
+                                      rerum inventore consectetur, neque doloribus, cupiditate
+                                      numquam dignissimos laborum fugiat deleniti? Eum quasi
+                                      quidem quibusdam."
+                        />
+                        <Review
+                          description="body1. Lorem ipsum dolor sit amet, consectetur adipisicing
+                                      elit. Quos blanditiis tenetur unde suscipit, quam beatae
+                                      rerum inventore consectetur, neque doloribus, cupiditate
+                                      numquam dignissimos laborum fugiat deleniti? Eum quasi
+                                      quidem quibusdam."
+                        />
+                        <Review
+                          description="body1. Lorem ipsum dolor sit amet, consectetur adipisicing
+                                      elit. Quos blanditiis tenetur unde suscipit, quam beatae
+                                      rerum inventore consectetur, neque doloribus, cupiditate
+                                      numquam dignissimos laborum fugiat deleniti? Eum quasi
+                                      quidem quibusdam."
+                        />
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                </Controls.Paper>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </div>
+
+      <Footer />
+    </div>
+  );
 };
 
 export default ProductView;
