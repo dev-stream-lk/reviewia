942414b9a21a9520db3249adbb12a82914d3fd75
Chamith Nimmitha <chamith@localhost.localdomain>
2021-07-11 23:46:34 +0530

    campare product page

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -15,52 +15,55 @@ const AboutUs = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.mainDiv}>
-      <Header/>
-
-      <Container>
-    <Grid container >
-      <Grid item xs={12} >
-        <Controls.Paper>
-          <Grid container >
-            <Grid item xs={12}>
-            <div >
-              <img alt="complex" src={Image} style={{maxHeight:"100%", maxWidth:"100%"}}/>
-            </div>
-              
-            </Grid>
-            <Grid item xs={12}>
-              <Grid container style={{textAlign:'center'}} alignItems="flex-start">
-                <Grid item xs={12}>
-                  <Typography variant="h5" style={{fontWeight:"bold"}}>About Us</Typography>
-                </Grid>
-                <Grid item xs={12}>
-                  <Typography variant="body1" gutterBottom>
-                    body1. Lorem ipsum dolor sit amet, consectetur adipisicing
-                    elit. Quos blanditiis tenetur unde suscipit, quam beatae
-                    rerum inventore consectetur, neque doloribus, cupiditate
-                    numquam dignissimos laborum fugiat deleniti? Eum quasi
-                    quidem quibusdam.
-                    body1. Lorem ipsum dolor sit amet, consectetur adipisicing
-                    elit. Quos blanditiis tenetur unde suscipit, quam beatae
-                    rerum inventore consectetur, neque doloribus, cupiditate
-                    numquam dignissimos laborum fugiat deleniti? Eum quasi
-                    quidem quibusdam.
-                  </Typography>
+    <>
+      <Header />
+      <Grid container>
+        <Grid item xs={12}>
+          <Controls.Paper>
+            <Grid container>
+              <Grid item xs={12}>
+                <div>
+                  <img
+                    alt="complex"
+                    src={Image}
+                    style={{ maxHeight: "100%", maxWidth: "100%" }}
+                  />
+                </div>
+              </Grid>
+              <Grid item xs={12}>
+                <Grid
+                  container
+                  style={{ textAlign: "center" }}
+                  alignItems="flex-start"
+                >
+                  <Grid item xs={12}>
+                    <Typography variant="h4" style={{ marginBottom:20 }}>
+                      About Us
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Typography align="left" variant="body1" gutterBottom>
+                      body1. Lorem ipsum dolor sit amet, consectetur
+                      adipisicing elit. Quos blanditiis tenetur unde suscipit,
+                      quam beatae rerum inventore consectetur, neque
+                      doloribus, cupiditate numquam dignissimos laborum fugiat
+                      deleniti? Eum quasi quidem quibusdam. body1. Lorem ipsum
+                      dolor sit amet, consectetur adipisicing elit. Quos
+                      blanditiis tenetur unde suscipit, quam beatae rerum
+                      inventore consectetur, neque doloribus, cupiditate
+                      numquam dignissimos laborum fugiat deleniti? Eum quasi
+                      quidem quibusdam.
+                    </Typography>
+                  </Grid>
                 </Grid>
               </Grid>
             </Grid>
-          </Grid>
-        </Controls.Paper>
+          </Controls.Paper>
+        </Grid>
       </Grid>
-    </Grid>
-  </Container>
-      <div style={{position:"fixed",bottom:0, left:0, right:0}}>
-        <Footer/>
-      </div>
-      
-    </div>
-  )
+      <Footer />
+    </>
+  );
 };
 
 export default AboutUs;
