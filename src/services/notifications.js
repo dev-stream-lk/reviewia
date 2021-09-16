bfb4f7a15c9b0dd0924855441578c20a3287bf40
Chamith Nimmitha <chamith@localhost.localdomain>
2021-09-17 01:22:47 +0530

    Notification panel

--- src/services/notifications.js
+++ src/services/notifications.js
@@ -4,7 +4,7 @@ import {getItem, setItem} from '../services/localStorage';
 const TOKEN = getItem("token");
 
 
-// get user notifications
+// get user notifications count
 export const getNotificationCount = (email) => {
 
 
@@ -17,6 +17,29 @@ export const getNotificationCount = (email) => {
     }
 
     return fetch(HOST+`user/notification/count?email=${email}`, requestOptions )
+    .then( async res => {
+        if(res.ok){
+            return await res.json();
+        }
+        return false;
+        
+    })
+    .catch( err=> console.error(err));
+}
+
+// get user notifications
+export const getNotifications = (email) => {
+
+    
+    const requestOptions = {
+        method:"GET",
+        headers:{
+          'Content-Type':"application/json",
+          'Authorization': `Bearer ${TOKEN}`
+        }
+    }
+
+    return fetch(HOST+`user/notification?email=${email}`, requestOptions )
     .then( async res => {
         if(res.ok){
             return await res.json();
